e3eebe07f6657c9ade46f7b59748cad8141c5433
Julien Richard-Foy <julien@richard-foy.fr>
2019-11-04 17:16:45 +0100

    Generalize the syntax of function types
    Fixes #23

--- src/main/scala/scalatutorial/sections/HigherOrderFunctions.scala
+++ src/main/scala/scalatutorial/sections/HigherOrderFunctions.scala
@@ -80,6 +80,12 @@ object HigherOrderFunctions extends ScalaTutorialSection {
    *
    * So, `Int => Int` is the type of functions that map integers to integers.
    *
+   * Similarly, `(A1, A2) => B` is the type of functions that take two arguments
+   * (of types `A1` and `A2`, respectively) and return a result of type `B`.
+   *
+   * More generally, `(A1, ..., An) => B` is the type of functions that take `n`
+   * arguments (of types `A1` to An`) and return a result of type `B`.
+   *
    * = Anonymous Functions =
    *
    * Passing functions as parameters leads to the creation of many small functions.
