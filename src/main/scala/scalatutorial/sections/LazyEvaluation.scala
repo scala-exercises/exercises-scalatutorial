c75a8918e1349365ad596d9ea920997e7bd07e80
47erbot <developer@47deg.com>
2021-09-29 05:23:42 +0000

    Reformat with scalafmt 3.0.5

--- src/main/scala/scalatutorial/sections/LazyEvaluation.scala
+++ src/main/scala/scalatutorial/sections/LazyEvaluation.scala
@@ -237,9 +237,9 @@ object LazyEvaluation extends ScalaTutorialSection {
   def lazyVal(res0: String): Unit = {
     val builder = new StringBuilder
 
-    val x = { builder += 'x'; 1 }
+    val x      = { builder += 'x'; 1 }
     lazy val y = { builder += 'y'; 2 }
-    def z = { builder += 'z'; 3 }
+    def z      = { builder += 'z'; 3 }
 
     z + y + x + z + y + x
 
